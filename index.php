35a6468aab52eed65786234e579f98ebb61d1099
AndreLPDZ <webdre@gmail.com>
2020-03-12 11:31:23 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    
+#Aqui PHP
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="PT/BR">
+    <head>
+        <meta charset="utf-8">
+        <title>Sistema de Votação</title>
+        <meta name="description" content="Sistema de Votação com PHP">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <form>
+        <!-- i -->
+        </form>
+    </body>
+</html>
