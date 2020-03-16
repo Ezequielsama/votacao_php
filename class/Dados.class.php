edfee9c978c81afe59b2582c5e87dff8349cf178
AndreLPDZ <webdre@gmail.com>
2020-03-16 16:28:07 -0300

    Create Dados.class.php

---
+++ class/Dados.class.php
@@ -0,0 +1,14 @@
+<?php
+# Não mostrar erros
+//error_reporting(0);
+
+class Dados {
+	# Atributos		
+	protected static $h = "???"; //-> localhost
+	protected static $u = "???"; //-> Usuário
+	protected static $s = "???"; //-> Senha
+	protected static $b = "???"; //-> Nome do banco	
+	# Método construtor
+	protected function __construct(){}	
+}
+?>
